f68287cba3233d17365198aebf21a392f7f254d5
VanceDR <vancedelrosario@yahoo.com>
2025-03-08 18:19:20 +0800

    prettify formatting and code - searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -13,10 +13,10 @@ router.get('/', async (req, res, next) => {
     const collection = db.collection('secondChanceItems')
 
     // Initialize the query object
-    let query = {}
+    const query = {}
 
     // Add the name filter to the query if the name parameter is not empty
-    if (req.query.name && req.query.name.trim() != '') {
+    if (req.query.name && req.query.name.trim() !== '') {
       query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
 
